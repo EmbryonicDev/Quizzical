724a6460e52ec2e15486735b8e39fbb0b37ceddc
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-19 14:40:06 +0200

    Add state: triviaData + useEffect to get trivia api data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import StartQuiz from "./components/StartQuiz";
 import blob1 from './Assets/blob5.png';
 import blob2 from './Assets/blobs.png';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 function App() {
   const [firstGame, setFirstGame] = useState(true);
+  const [triviaData, setTriviaData] = useState([]);
 
   function startFirstGame() {
     setFirstGame(prevState => !prevState)
+    console.log(triviaData)
   }
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&category=9")
+      .then(res => res.json())
+      .then(data => setTriviaData(data.results))
+  }, [firstGame])
 
   return (
     <div className="App">
