e8e16addc1ce5ef51263269e3cb21b5f62c84b8f
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-19 14:17:07 +0200

    Fix typo: quizical -> quizzical

--- src/components/StartQuiz.js
+++ src/components/StartQuiz.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function StartQuiz(props) {
   return (
     <div id='startQuiz'>
-      <h1 id='startQuiz--title'>Quizical</h1>
+      <h1 id='startQuiz--title'>Quizzical</h1>
       <p id='startQuiz--description'>
         Test your knowledge on details, considerations, & pieces of information of little importance or value 😋...
       </p>
