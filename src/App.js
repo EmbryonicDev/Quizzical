8ce73d9347be5dd34b13454db58f680810b526cd
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-19 14:12:29 +0200

    Add firstGame state to App & set to true

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import StartQuiz from "./components/StartQuiz";
 import blob1 from './Assets/blob5.png';
 import blob2 from './Assets/blobs.png';
+import { useState } from "react";
 
 
 function App() {
+  const [firstGame, setFirstGame] = useState(true);
+
   return (
     <div className="App">
       <img className="blob1" src={blob1} alt=""></img>
