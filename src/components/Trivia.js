7ba1a20ef49173f64c978e3a74e9b6cb12470f0a
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-20 12:21:29 +0200

    Use map to keep code dry for creating answer buttons

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import uniqid from 'uniqid';
 
 export default function Trivia(props) {
+  const buttonElms = props.allAnswers.map(answer =>
+    <button className='answerBtn' key={uniqid()}>{answer}</button>
+  );
+
   return (
     <div id='trivia'>
       <h4 className='trivia--question' >{props.question}</h4>
       <div className='trivia--answers'>
-        <button className='answerBtn'>{props.answers[0]}</button>
-        <button className='answerBtn'>{props.answers[1]}</button>
-        <button className='answerBtn'>{props.answers[2]}</button>
-        <button className='answerBtn'>{props.answers[3]}</button>
+        {buttonElms}
       </div>
     </div>
   )
