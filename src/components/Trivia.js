edf54886241185b677c0c270956a09864c823954
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-21 15:20:35 +0200

    Remove commented out code from Trivia

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,29 +1,4 @@
 export default function Trivia(props) {
-
-  // function unSelectAll() {
-  //   // clear all isSelected first to avoid multiple
-  //   //  answers being selected
-  //   setAnswers(prevState => prevState.map(answer => {
-  //     return answer.isSelected ?
-  //       { ...answer, isSelected: false } :
-  //       answer
-  //   }))
-  // }
-
-  // function select(id) {
-  //   unSelectAll()
-
-  //   // Highlight selected answer
-  //   setAnswers(prevState => prevState.map(answer => {
-  //     console.log('clicked ' + id)
-  //     return (
-  //       answer.id === id ?
-  //         { ...answer, isSelected: !answer.isSelected } :
-  //         answer
-  //     )
-  //   }))
-  // }
-
   return (
     <div id='trivia'>
       <h4 className='trivia--question' >{props.question}</h4>
